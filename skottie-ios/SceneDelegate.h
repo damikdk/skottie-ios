d2f67f6898f45113a9a45a9cbd7a5aa735e3cf8f
Damik Minnegalimov <damikdk@gmail.com>
2023-04-26 16:07:34 +0300

    Init commit

---
+++ skottie-ios/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  skottie-ios
+//
+//  Created by Damir Minnegalimov on 27.03.2023.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
